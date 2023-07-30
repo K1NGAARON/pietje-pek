5f563fafcd6da6dbbc09e1b1c50b4fbd3f1fc839
K1NGAARON <gerardaaron1997@gmail.com>
2023-07-30 14:56:07 +0200

    header module

--- js/global.js
+++ js/global.js
@@ -1,10 +1,13 @@
 $(document).scroll(function () {
     const header = $('.header');
+    const logo = $('.header .logo');
 
     if ($(this).scrollTop() > header.height()) {
         header.addClass('active');
+        $(logo).attr("src","/assets/logo/logo-black.png");
     } else {
         header.removeClass('active');
+        $(logo).attr("src","/assets/logo/logo.png");
     }
 });
 
@@ -17,6 +20,19 @@ function closeMenu() {
 };
 
 $('.small-menu-content .nav').click(closeMenu);
+
+
+// REFACTOR THIS BELOW
+function toggleMenuDropdown(e) {
+    if ($(this).parent().hasClass("active")) {
+        $(this).parent().removeClass('active');
+    } else {
+        $('.dropdown-btn').parent().removeClass('active');
+        $(this).parent().addClass('active');
+    }
+};
+
+$('.dropdown-btn').click(toggleMenuDropdown);
 
 const cookieBanner = document.querySelector(".cookie-banner-wrapper");
 const cookiesAccepted = document.querySelector("#accept-cookies");
