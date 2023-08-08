0becaab017515dd429555fabe05a8be849c0d82f
K1NGAARON <gerardaaron1997@gmail.com>
2023-08-08 14:03:12 +0200

    added reviews

--- home/js/script.js
+++ home/js/script.js
@@ -1,13 +1,25 @@
 function toggleReview(e) {
     const reviews = [
         {
-            body: "review 1 text",
+            body: `
+            Very pleasant experience.
+            <br>
+            Ordered mixed meat oil fondue and its our first meal here in Brugge, late at night. Very nice end to a long day of travel.
+            <br><br>
+            The meat was fresh and a portion for 2 ppl was enough for the three  of us. With beer and the salad and fries, we are really full! And we are big eaters!
+            <br><br> 
+            We were pleasantly served by a young lady, very attentive and easy to talk to. Its a very nice meal. Thanks!
+            `,
         },
         {
-            body: "review 2 text",
+            body: `
+            Great place, tasty food! Literally 3/4mins walking from the main square! The prawns were one of the best I've tried(I am a prawn lover, so ^^ ) and the girls are very nice and friendly! An easy 10/10 from my wife and I.
+            `
         },
         {
-            body: "review 3 text",
+            body: `
+            A really good typical Belgian restaurant in the centre of the beautiful Bruges. The food was really good, very tasty meat and good beer. Definitely a place to go if you want to eat a good dinner in Bruges
+            `,
         },
     ];
 
@@ -18,11 +30,11 @@ function toggleReview(e) {
     const target = document.querySelector('#review-body');
 
     if (clickedBtn === 'review1') {
-        $(target).text(reviews[0].body);
+        $(target).html(reviews[0].body);
     } else if (clickedBtn === 'review2') {
-        $(target).text(reviews[1].body);
+        $(target).html(reviews[1].body);
     } else if (clickedBtn === 'review3') {
-        $(target).text(reviews[2].body);
+        $(target).html(reviews[2].body);
     }
 };
 
